1327a492b6030b11ee7239480b72a459318a7bfd
Radosław Lechowicz <radzio9995@wp.pl>
2019-09-21 16:17:18 +0200

    First commit

---
+++ src/main/java/pl/rlechowicz/sklep/SklepApplication.java
@@ -0,0 +1,13 @@
+package pl.rlechowicz.sklep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SklepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SklepApplication.class, args);
+	}
+
+}
